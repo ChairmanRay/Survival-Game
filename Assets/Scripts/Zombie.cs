63fba667e83c7850205661bbb0d87383f0e18a3a
ChairmanRay <rswang@outlook.com>
2015-05-24 15:33:28 -0700

    Simple Zombie AI
    Added a simple zombie AI that follows the player around

---
+++ Assets/Scripts/Zombie.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zombie : MonoBehaviour {
+
+	float sightRange = 25;
+	float attackRange = 10;
+	float moveSpeed = 5;
+	Transform target = null;
+	NavMeshAgent nav;
+	
+	void Awake () {
+		nav = GetComponent <NavMeshAgent> ();
+	}
+
+	void Update () {
+		if (target == null && GameObject.FindGameObjectWithTag ("Player")) {
+			target = GameObject.FindGameObjectWithTag ("Player").transform;
+		} 
+		else if (target)
+		{
+			nav.SetDestination (target.position);
+		}
+	}
+}
