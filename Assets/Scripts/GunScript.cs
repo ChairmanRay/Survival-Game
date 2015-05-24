5770c9da0f0af799ec84a6057a6509ab04bf243f
zombhe <zombhe1@gmail.com>
2015-05-24 14:46:00 -0600

    Start for gun script

--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -3,8 +3,13 @@ using System.Collections;
 
 public class GunScript : MonoBehaviour {
 	
-	public Vector3 LocalHipPosition;
-	public Vector3 LocalAimPosition;
+	public Vector3 LocalHipPosition; //Position of the gun when not aiming
+	public Vector3 LocalAimPosition; //Position of the gun when aiming
+	
+	public float FOVwhenAiming; //How will the camera zoom
+	
+	public int BulletsInClip; //How many bullets does this gun hold
+	public float FiringSpeed; //How fast does the gun shoot
 	
 	void Start ()
 	{
